b4609c4fe587d061be09b7bfa1ef3efdd86d38c3
Latiffhl <latiffalikal@gmail.com>
2025-09-08 08:48:31 +0700

    responsive

--- src/pages/TentangPerusahaan.tsx
+++ src/pages/TentangPerusahaan.tsx
@@ -13,7 +13,7 @@ const TentangPerusahaan = () => {
           <img src="\assets\img\item\bg-1.png" alt="Modern office building" className="w-full h-full object-cover opacity-90" />
         </div>
         <div className="absolute bottom-0 right-12 mr-10 transform translate-x-1/4 translate-y-1/4">
-          <img src="\assets\img\item\icon-1.png" alt="About Us" className="w-48 h-48 object-contain" />
+          <img src="\assets\img\item\icon-1.png" alt="About Us" className="w-36 sm:w-36 md:w-48 object-contain" />
         </div>
       </div>
 
